892ba433ea27e2088dac60730b6e14c4ef3d6773
theplas <kanicepakulate@gmail.com>
2020-09-28 11:57:22 +0700

    hello

---
+++ Book.cs
@@ -0,0 +1,9 @@
+namespace DictionaryThemSuaXoaTimkiem
+{
+     class Book
+    {
+    public string ISBN { get; set; }
+    public string Name { get; set; }
+    public string Author { get; set; }
+    }
+}
